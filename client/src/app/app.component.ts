d6e6155e337428a00907ca3180beaf1917f04ab7
nina1valkova <ninavalkova2001@gmail.com>
2024-11-07 22:50:39 +0200

    section 9

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,31 +1,43 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
 import { Product } from './shared/models/product';
 import { Pagination } from './shared/models/pagination';
+import { ShopService } from './core/services/shop.service';
+import { ShopComponent } from "./features/shop/shop.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent
+export class AppComponent 
 {
+  title = 'Skinet';
+
+  //private shopService = inject(ShopService);
+  /*
   baseUrl = 'https://localhost:5025/api/'
   private http = inject(HttpClient);
-  title = 'Skinet';
-  products: Product[] = [];
+  */
 
-  ngOnInit():void
-  {
-    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+   //ince same tova ostava no sega go metia drugase v shopcomonent
+      //title = 'Skinet';
+      // products: Product[] = [];
+
+      //ince same tova ostava no sega go metia drugase v shopcomonent
+ // ngOnInit():void
+ // {
+    /* this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe(*/
+   // this.shopService.getProducts().subscribe({
       //next: data => console.log(data),
-      next: response => this.products = response.data,
-      error: error =>console.log(error),
-      complete: () => console.log('complete')
-    })
-  }
+   //   next: response => this.products = response.data,
+    //  error: error =>console.log(error)
+      /*complete: () => console.log('complete')*/
+   // })
+    
+  //}
 }
---
+++ client/src/app/core/services/shop.service.ts
@@ -0,0 +1,70 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core'
+import { Product } from '../../shared/models/product';
+import { Pagination } from '../../shared/models/pagination';
+import { ShopParams } from '../../shared/models/shopParams';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShopService {
+  baseUrl = 'https://localhost:5025/api/'
+  private http = inject(HttpClient);
+  types: string[] = [];
+  brands: string[] = [];
+
+  getProducts(/*brands?: string[], types?: string[], sort?: string*/ shopParams: ShopParams) {
+    let params = new HttpParams();
+
+    if(/*brands && brands.length >0*/shopParams.brands.length>0)
+    {
+       params = params.append('brands', /*brands.join(',')*/ shopParams.brands.join(','));
+    }
+
+    if(/*types && types.length >0*/shopParams.types.length>0)
+    {
+      params = params.append('types', /*types.join(',')*/shopParams.types.join(','));
+    }
+
+    if(/*sort*/shopParams.sort)
+    {
+      params = params.append('sort', /*sort*/ shopParams.sort);
+    }
+
+    if(shopParams.search)
+    {
+      params = params.append('search', shopParams.search);
+    }
+
+
+    params = params.append('pageSize',shopParams.pageSize);
+    //pageIndex - is in my APi is pageNumber
+    params = params.append('pageIndex', shopParams.pageNumber);
+
+                                             //query string for http request     
+    return this.http.get<Pagination<Product>>(this.baseUrl + 'products' , {params})
+  }
+
+  getBrands() {
+    if (this.brands.length > 0) {
+      return
+    }
+
+    return this.http.get<string[]>(this.baseUrl + 'products/brands').subscribe(
+      {
+        next: response => this.brands = response
+      })
+  }
+
+  getTypes() {
+    if(this.types.length>0)
+    {
+      return
+    }
+    return this.http.get<string[]>(this.baseUrl + 'products/types').subscribe({
+      next: response => this.types = response
+    })
+  }
+
+}
